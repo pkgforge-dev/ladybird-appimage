214f91d25a0e23368fd067e553bc3ce7d99c4291
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-05 11:38:50 -0400

    update to latest template (#14)
    * update to template
    
    * update README
    
    * build weekly
    
    * add more packages
    
    * fix?
    
    * disable `aarch64`
    
    * simple-test
    
    * bump `LATEST_VERSION` [skip ci]
    
    * fix
    
    * try `--test` again
    
    * bump `LATEST_VERSION` [skip ci]
    
    * fix
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ get-dependencies.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+# pacman -Syu --noconfirm PACKAGESHERE
+
+if [ "$ARCH" = 'x86_64' ]; then
+		pacman -Syu --noconfirm libva-intel-driver
+fi
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini ffmpeg-mini
+
+# Comment this out if you need an AUR package
+pacman -Rdd --noconfirm libpng || :
+make-aur-package --chaotic-aur ladybird
+
+# If the application needs to be manually built that has to be done down here
+
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
---
+++ make-appimage.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q ladybird | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Base/res/icons/128x128/app-browser.png
+export DESKTOP=https://raw.githubusercontent.com/LadybirdBrowser/ladybird/refs/heads/master/Meta/CMake/freedesktop/org.ladybird.Ladybird.desktop
+export ANYLINUX_LIB=1
+
+# Deploy dependencies
+quick-sharun \
+	/opt/ladybird/usr/bin/*          \
+	/opt/ladybird/usr/lib/*          \
+	/opt/ladybird/usr/lib/ladybird/* \
+	/opt/angle/usr/lib/*             \
+	/opt/ladybird/usr/share/*
+
+mv -v ./AppDir/lib/angle/usr/lib/* ./AppDir/lib
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
