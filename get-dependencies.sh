c5aefb44a50a319eff377b77210ea35a0f66723e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-04 22:10:40 -0400

    add more packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,9 +8,13 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 # pacman -Syu --noconfirm PACKAGESHERE
 
+if [ "$ARCH" = 'x86_64' ]; then
+		pacman -Syu --noconfirm libva-intel-driver
+fi
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini ffmpeg-mini
 
 # Comment this out if you need an AUR package
 make-aur-package --chaotic-aur ladybird
